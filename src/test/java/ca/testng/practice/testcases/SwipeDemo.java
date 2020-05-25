5ed03bb6cda4f7c7c85d1a82f6f045c2cc32d194
ekotliar <elena.kotliar@corusent.com>
2020-05-25 14:53:21 -0400

    push to git

--- src/test/java/ca/testng/practice/testcases/SwipeDemo.java
+++ src/test/java/ca/testng/practice/testcases/SwipeDemo.java
@@ -1,8 +1,13 @@
 package ca.testng.practice.testcases;
 import java.net.MalformedURLException;
+import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 import io.appium.java_client.TouchAction;
@@ -20,7 +25,22 @@ public class SwipeDemo extends Base {
         skipButton.click();
 
         //or
-        driver.findElementByAccessibilityId("com.shawmedia.smglobal:id/left_button").click();
+        //driver.findElementByAccessibilityId("com.shawmedia.smglobal:id/left_button").click();
+        //or
+        //driver.findElement(MobileBy.AndroidUIAutomator("text(\"SKIP\")")).click();
+
+        // calculate bottom & top of the screen
+        Dimension size = driver.manage().window().getSize();
+        int middleX = (int) (size.getWidth() * 0.5);
+        int bottomY = (int) (size.getHeight() * 0.8);
+        int topY = (int) (size.getHeight() * 0.3);
+// lookup for element to refresh appium
+        List<AndroidElement> lst = driver.findElementsById("com.shawmedia.smglobal:id/shows_grid_view");
+        System.out.println(lst.size());
+// scroll screen
+        new TouchAction(driver).press(PointOption.point(middleX, bottomY))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000))).moveTo(PointOption.point(middleX, topY)).release()
+                .perform();
 
 
     }
