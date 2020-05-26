ca93fe64c8ee3fc17f85748ac300027f4a58e37c
ekotliar <elena.kotliar@corusent.com>
2020-05-26 09:25:42 -0400

    push to git

--- src/test/java/ca/testng/practice/testcases/Base.java
+++ src/test/java/ca/testng/practice/testcases/Base.java
@@ -1,11 +1,17 @@
 package ca.testng.practice.testcases;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+
 public class Base {
     public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException, InterruptedException {
         File fs=new File("/Users/ekotliar/Project/QA_Practice/src/test/resources/applications/android-release-build-4.0.0-405.apk");
@@ -16,6 +22,7 @@ public class Base {
         cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
         AndroidDriver<AndroidElement> driver=new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
         Thread.sleep(5000);
+
         return driver;
     }
 }
--- src/test/java/ca/testng/practice/testcases/SwipeDemo.java
+++ src/test/java/ca/testng/practice/testcases/SwipeDemo.java
@@ -1,12 +1,14 @@
 package ca.testng.practice.testcases;
 import java.net.MalformedURLException;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
@@ -18,7 +20,7 @@ import static io.appium.java_client.touch.offset.ElementOption.element;
 public class SwipeDemo extends Base {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
         // TODO Auto-generated method stub
-        AndroidDriver<AndroidElement> driver= Capabilities();
+        AndroidDriver<AndroidElement> driver = Capabilities();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         WebElement skipButton = driver.findElement(MobileBy.AndroidUIAutomator("text(\"SKIP\")"));
@@ -29,6 +31,20 @@ public class SwipeDemo extends Base {
         //or
         //driver.findElement(MobileBy.AndroidUIAutomator("text(\"SKIP\")")).click();
 
+
+        //navigate to Shows - Global brand
+        driver.findElementByAccessibilityId("global").click();
+        //or
+        //driver.findElementByXPath("//android.widget.ImageView[@content-desc='history']").click()
+        Thread.sleep(2000);
+//click on show to get show details page
+        WebElement show = driver.findElement(MobileBy.AndroidUIAutomator("description(\"A Little Late with Lilly Singh\")"));
+        show.click();
+        Thread.sleep(2000);
+//click Back button to return to Shows screen - Global brand
+        driver.findElementById("com.shawmedia.smglobal:id/detail_page_back_button").click();
+
+// scroll screen
         // calculate bottom & top of the screen
         Dimension size = driver.manage().window().getSize();
         int middleX = (int) (size.getWidth() * 0.5);
@@ -37,11 +53,21 @@ public class SwipeDemo extends Base {
 // lookup for element to refresh appium
         List<AndroidElement> lst = driver.findElementsById("com.shawmedia.smglobal:id/shows_grid_view");
         System.out.println(lst.size());
-// scroll screen
         new TouchAction(driver).press(PointOption.point(middleX, bottomY))
                 .waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000))).moveTo(PointOption.point(middleX, topY)).release()
                 .perform();
-
-
+        new TouchAction(driver).press(PointOption.point(middleX, bottomY))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000))).moveTo(PointOption.point(middleX, topY)).release()
+                .perform();
+        WebElement show1 = driver.findElement(MobileBy.AndroidUIAutomator("description(\"EVIL\")"));
+        show1.click();
+//click Shows navigation button to return to Shows screen - Global brand
+        WebElement NavShow = driver.findElement(MobileBy.AndroidUIAutomator("text(\"SHOWS\")"));
+        NavShow.click();
+        //driver.findElementById("com.shawmedia.smglobal:id/detail_page_back_button").click();
     }
 }
+
+
+
+
